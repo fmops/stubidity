981ecdb6a207b2147c726ab771050c1f9cb7e860
Feynman Liang <feynman@blueteam.ai>
2024-08-14 14:58:38 -0700

    Release 0.4.0 oai spec

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Stubidity.MixProject do
   def project do
     [
       app: :stubidity,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
